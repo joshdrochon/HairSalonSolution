920a84b096a30667b0e7c3c4ee69bf983e6f1863
Josh Rochon <joshdrochon@gmail.com>
2018-02-23 11:00:18 -0800

    create GetAll_DatabaseEmptyAtFirst test

---
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using HairSalonProject.Models;
+using HairSalonProject;
+using System;
+
+namespace HairSalonProject.Tests
+{
+  [TestClass]
+  public class StylistTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Stylist.DeleteAll();
+    }
+
+    public StylistTests()
+    {
+      DBConfiguration.ConnectionString =
+      "server=localhost;user id=root;password=root;port=8889;database=world_test;";
+    }
+
+
+    [TestMethod]
+    public void GetAll_DatabaseEmptyAtFirst_0()
+    {
+      //Arrange, Act
+      int result = Stylist.GetAll().Count;
+      Console.WriteLine("Line 29 " + result);
+      //Assert
+      Assert.AreEqual(0, result);
+    }
+
+
+  }
+}
